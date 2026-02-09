61a8d052999a04fb5873fd5ca7d4ad153b77cd65
Ajay <gangwar030@gmail.com>
2026-02-09 22:16:18 +0530

    Contains Duplicate_problem 217

---
+++ Leetcode/Easy/Contains_Duplicate_217.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+class Solution {
+
+    // Method 1 with brute force but it will not a valid solution
+    public boolean method_1(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+
+    }
+
+    // METHOD 2 BY USING LIST
+    public boolean method_2(int[] nums) {
+
+        List<Integer> ls = new ArrayList<>();
+
+        for (Integer arr : nums) {
+            if (ls.contains(arr)) {
+                return true;
+            } else {
+                ls.add(arr);
+            }
+        }
+        return false;
+    }
+
+    
+}
+
+public class Contains_Duplicate_217 {
+    public static void main(String args[]) {
+        Solution s = new Solution();
+
+        // Test cases....
+        // int [] nums = {1, 2, 3, 1};
+        // int [] nums = {1, 2, 3, 4};
+        int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+
+        // s.containsDuplicate(nums);
+        System.out.println(s.method_1(nums));
+        
+
+    }
+}

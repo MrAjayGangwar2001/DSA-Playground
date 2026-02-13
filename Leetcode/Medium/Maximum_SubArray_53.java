326ad3f2ef3ded5a8ec150bfd0fe3c7c5a962a47
Ajay <gangwar030@gmail.com>
2026-02-13 23:31:11 +0530

    Added if Condition_Maximum_SubArray

--- Leetcode/Medium/Maximum_SubArray_53.java
+++ Leetcode/Medium/Maximum_SubArray_53.java
@@ -1,40 +1,42 @@
 package Leetcode.Medium;
 
-class Problem{
+class Problem {
 
-    public int SubArray(int [] nums){
+    // Optimize Approach by Using Kadane's Algorithm in O(n)
+    public int SubArray(int[] nums) {
 
         int max_sum = nums[0];
         int curr_sum = 0;
 
-        if(nums.length == 1) return max_sum;
-
-        // for (int arr : nums) {
-            for(int i = 0; i < nums.length; i++){
-
-                curr_sum = Math.max(nums[i], curr_sum + nums[i]);
-                max_sum = Math.max(max_sum, curr_sum);
-            }
-        
+        if (nums.length == 1)
             return max_sum;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            curr_sum = Math.max(nums[i], curr_sum + nums[i]);
+
+            if (curr_sum < 0) {
+                curr_sum = 0;
+            }
+            max_sum = Math.max(max_sum, curr_sum);
+        }
+
+        return max_sum;
     }
 }
 
 public class Maximum_SubArray_53 {
 
     public static void main(String[] args) {
-        
-        int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+        int nums[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 
         Problem pb = new Problem();
 
         System.out.println(pb.SubArray(nums));
-        
+
     }
 }
-
-
-
 
 // nums =
 // [-2,1,-3,4,-1,2,1,-5,4]

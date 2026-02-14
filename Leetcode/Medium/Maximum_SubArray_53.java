4c0aebd6401320d9f2309ed4093143ffb461f2fa
Ajay <gangwar030@gmail.com>
2026-02-15 01:46:11 +0530

    Maximum SubArray problem

--- Leetcode/Medium/Maximum_SubArray_53.java
+++ Leetcode/Medium/Maximum_SubArray_53.java
@@ -37,3 +37,35 @@ public class Maximum_SubArray_53 {
 
     }
 }
+
+
+// Leetcode Submitted Code...
+
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int max_sum = nums[0];
+//         int curr_sum = 0;
+//         if (nums.length == 1) {
+//             return nums[0];
+//         }
+//         // if(nums.length == 2){
+//         //     int max = Math.max(nums[0], nums[0] + nums[1]);
+//         //     return max; 
+//         // }
+//         // for (int arr : nums) {
+//         for (int i = 0; i < nums.length; i++) {
+//             // int reset = 0;
+//             curr_sum = Math.max(nums[i], curr_sum + nums[i]);
+//             // curr_sum = curr_sum + nums[i];
+//             // if (curr_sum < 0) {
+//             //     curr_sum = 0;
+//             //     // max_sum = 0;
+//             // }
+
+//             max_sum = Math.max(max_sum, curr_sum);
+//         }
+//         return max_sum;
+//     }
+
+// }

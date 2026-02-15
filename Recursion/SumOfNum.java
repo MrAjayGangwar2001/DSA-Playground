64d495f89c9e1a39c23801a4aedc9d814d3c7fd8
Ajay <gangwar030@gmail.com>
2026-02-16 00:00:53 +0530

    Sum of Nums

---
+++ Recursion/SumOfNum.java
@@ -0,0 +1,15 @@
+public class SumOfNum {
+
+    public static void sum(int i, int n, int sum){
+        if (i == n) {
+            sum += i;
+            System.out.println("Sum of All Numbers : " +sum);
+            return;
+        }
+        sum += i;
+        sum(i+1, n, sum);
+    }
+    public static void main(String[] args) {
+        sum(1, 15, 0);
+    }
+}

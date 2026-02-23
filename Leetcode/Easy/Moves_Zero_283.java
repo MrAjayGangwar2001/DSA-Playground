9bb31294ff5575648e1a5a0ea5487254c0da3055
Ajay <gangwar030@gmail.com>
2026-02-24 01:05:14 +0530

    By Method 1 -> Brute force Method

--- Leetcode/Easy/Moves_Zero_283.java
+++ Leetcode/Easy/Moves_Zero_283.java
@@ -1,25 +1,32 @@
-package Leetcode.Easy;
 
 public class Moves_Zero_283 {
     public static void main(String[] args) {
 
-        int arr[] = { 1, 0, 3, 6, 9, 0, 8, 7 };
+        // int arr[] = { 1, 0, 3, 6, 9, 0, 8, 7};
+        // int arr[] = { 0, 0, 1 };
+        int arr[] = { 0, 1, 0, 3, 12 };
 
-        for (int i = 0; i < arr.length;) {
+        // This forloop Methods is Working but Taking time longer than expected or Exceed Maximum
 
-            if (arr[i] == 0) {
+        for (int k = 0; k < arr.length; k++) {
 
-                for (int j = i; j < (arr.length) - 1; j++) {
+            for (int i = 0; i < arr.length;) {
 
-                    arr[j] = arr[j + 1];
-                    // arr[i + 1] = (arr.length) - 1;
-                    // System.out.println(arr[i]);
+                if (arr[i] == 0) {
 
+                    for (int j = i; j < (arr.length) - 1; j++) {
+
+                        arr[j] = arr[j + 1];
+                        // arr[i + 1] = (arr.length) - 1;
+                        // System.out.println(arr[i]);
+
+                    }
+                    arr[arr.length - 1] = 0;
                 }
+                i++;
             }
-            arr[arr.length - 1] = 0;
-            i++;
         }
+
         for (int nums : arr) {
             System.out.print(nums + ", ");
         }

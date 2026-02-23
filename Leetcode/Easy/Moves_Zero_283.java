7b6b9de68c12bb3cdd1eae760e7f4b567582ba84
Ajay <gangwar030@gmail.com>
2026-02-24 01:12:58 +0530

    By Method 2 -> Optimized(Two Pointer) Method

--- Leetcode/Easy/Moves_Zero_283.java
+++ Leetcode/Easy/Moves_Zero_283.java
@@ -2,29 +2,46 @@
 public class Moves_Zero_283 {
     public static void main(String[] args) {
 
-        // int arr[] = { 1, 0, 3, 6, 9, 0, 8, 7};
+        int arr[] = { 1, 0, 3, 6, 9, 0, 8, 7 };
         // int arr[] = { 0, 0, 1 };
-        int arr[] = { 0, 1, 0, 3, 12 };
+        // int arr[] = { 0, 1, 0, 3, 12 };
 
-        // This forloop Methods is Working but Taking time longer than expected or Exceed Maximum
+        // This forloop Methods is Working but Taking time longer than expected or
+        // Exceed Maximum
+        /*
+         * for (int k = 0; k < arr.length; k++) {
+         * 
+         * for (int i = 0; i < arr.length;) {
+         * 
+         * if (arr[i] == 0) {
+         * 
+         * for (int j = i; j < (arr.length) - 1; j++) {
+         * 
+         * arr[j] = arr[j + 1];
+         * // arr[i + 1] = (arr.length) - 1;
+         * // System.out.println(arr[i]);
+         * 
+         * }
+         * arr[arr.length - 1] = 0;
+         * }
+         * i++;
+         * }
+         * }
+         */
 
-        for (int k = 0; k < arr.length; k++) {
+        // Method 2:- Optimized Approach(Two Pointer)
 
-            for (int i = 0; i < arr.length;) {
+        int idx = 0;
 
-                if (arr[i] == 0) {
-
-                    for (int j = i; j < (arr.length) - 1; j++) {
-
-                        arr[j] = arr[j + 1];
-                        // arr[i + 1] = (arr.length) - 1;
-                        // System.out.println(arr[i]);
-
-                    }
-                    arr[arr.length - 1] = 0;
-                }
-                i++;
+        for (int num : arr) {
+            if (num != 0) {
+                arr[idx++] = num;
             }
+
+        }
+
+        while (idx < arr.length) {
+            arr[idx++] = 0;
         }
 
         for (int nums : arr) {

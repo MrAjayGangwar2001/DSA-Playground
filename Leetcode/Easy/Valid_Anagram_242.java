e86bafa53fa59c431f3deb0aa568a755e6b09f2a
Ajay <gangwar030@gmail.com>
2026-02-25 16:36:52 +0530

    Valid Anagram Method 1

--- Leetcode/Easy/Valid_Anagram_242.java
+++ Leetcode/Easy/Valid_Anagram_242.java
@@ -1,5 +1,42 @@
-public class Valid_Anagram_242{
+
+import java.util.Arrays;
+
+class Angrm {
+
+    // Method 1 : Time Complexity O(n log n)
+    public boolean check() {
+
+        // String s = "arc";
+        // String t = "car";
+        String s = "listen";
+        String t = "silent";
+
+        boolean Anagram = false;
+
+        char a[] = s.toCharArray();
+        char b[] = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (s.length() != t.length())
+            return Anagram;
+
+        if (Arrays.equals(a, b)) {
+
+            Anagram = true;
+            return Anagram;
+        } else {
+            return Anagram;
+        }
+
+    }
+}
+
+public class Valid_Anagram_242 {
     public static void main(String[] args) {
-        
+        Angrm ag = new Angrm();
+        System.out.println(ag.check());
+
     }
 }

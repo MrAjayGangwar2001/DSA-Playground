1c800411d8c95016ddb15314c13756c1b9c68032
Ajay <gangwar030@gmail.com>
2026-02-27 14:55:48 +0530

    Added Method 2 using HashMap

--- Leetcode/Easy/Valid_Anagram_242.java
+++ Leetcode/Easy/Valid_Anagram_242.java
@@ -1,15 +1,17 @@
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 class Angrm {
 
-    // Method 1 : Time Complexity O(n log n)
+    String s = "listen";
+    String t = "silent";
+
+    // Method 1 : Time Complexity O(n log n) & space O(n )
     public boolean Method_1() {
 
         // String s = "arc";
         // String t = "car";
-        String s = "listen";
-        String t = "silent";
 
         boolean Anagram = false;
 
@@ -31,12 +33,39 @@ class Angrm {
         }
 
     }
+
+
+    // Method : 2 by using HashMap
+
+    public boolean Method_2(){
+
+        if(s.length() != t.length()) return  false;
+
+        HashMap<Character, Integer> ls =  new HashMap<>();
+
+        for(char x : s.toCharArray()){
+            ls.put(x, ls.getOrDefault(x, 0) +1);
+
+        }
+
+        for (char y : t.toCharArray()) {
+            
+            if (!ls.containsKey(y) || ls.get(y) == 0) {
+                return false;
+            }
+
+            ls.put(y, ls.get(y) - 1);
+        }
+
+        return true;
+    }
 }
 
 public class Valid_Anagram_242 {
     public static void main(String[] args) {
         Angrm ag = new Angrm();
-        System.out.println(ag.Method_1());
+        // System.out.println(ag.Method_1());
+        System.out.println(ag.Method_2());
 
     }
 }

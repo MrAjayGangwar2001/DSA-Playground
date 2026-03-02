9aa6bae8d8f88adb1f2de173b60bd6dea4152001
Ajay <gangwar030@gmail.com>
2026-03-02 23:11:50 +0530

    Valid Palindrome String Problem 125

---
+++ Leetcode/Easy/Valid_Palindrome_Str_125.java
@@ -0,0 +1,33 @@
+
+class Palindrome{
+
+    public boolean check(){
+        // String s = "race a car";
+        String s = "A man, a plan, a canal: Panama";
+
+        String clean = s.replaceAll("[!@#$%^&*()_:,.]", "").toLowerCase();
+        String str = clean.replaceAll(" ", "");
+        StringBuilder sb = new StringBuilder(str);
+        String rev = sb.reverse().toString();
+
+        // System.out.println(str);
+        // System.out.println(rev);
+
+        if (str.equals(rev)) {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
+
+
+public class Valid_Palindrome_Str_125{
+    public static void main(String args[]){
+        
+        Palindrome p = new Palindrome();
+
+        System.out.println(p.check());
+    }
+}

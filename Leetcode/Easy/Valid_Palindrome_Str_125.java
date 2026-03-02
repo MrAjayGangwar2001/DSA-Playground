5c834010cefb1c6a21e80aa91705adf05e2f1757
Ajay <gangwar030@gmail.com>
2026-03-02 23:50:23 +0530

    Valid Palindrome String Problem 125 added some Regex and Another short option

--- Leetcode/Easy/Valid_Palindrome_Str_125.java
+++ Leetcode/Easy/Valid_Palindrome_Str_125.java
@@ -1,16 +1,21 @@
 
 class Palindrome{
 
-    public boolean check(){
+    public boolean Method_1(){
         // String s = "race a car";
-        String s = "A man, a plan, a canal: Panama";
+        // String s = "A man, a plan, a canal: Panama";
+        // String s = "Marge, let's \"[went].\" I await {news} telegram.";
+        
+        // String s = "Was it\\ a rat I saw?";
+        String s = "`l;`` 1o1 ??;l`";
 
-        String clean = s.replaceAll("[!@#$%^&*()_:,.<>?/\\|\"{}']", "").toLowerCase();
+        // String clean = s.replaceAll("[\\\\`!@#$%^&*()_:,;.<>?/\\|\"{}'--+=\\[\\]]", "").toLowerCase();
+        String clean = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
         String str = clean.replaceAll(" ", "");
         StringBuilder sb = new StringBuilder(str);
         String rev = sb.reverse().toString();
 
-        // System.out.println(str);
+        System.out.println(str);
         // System.out.println(rev);
 
         if (str.equals(rev)) {
@@ -28,6 +33,6 @@ public class Valid_Palindrome_Str_125{
         
         Palindrome p = new Palindrome();
 
-        System.out.println(p.check());
+        System.out.println(p.Method_1());
     }
 }

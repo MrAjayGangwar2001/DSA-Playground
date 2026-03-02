588c53aed85132a8a75a6cd4b33a60ac0a9da0d9
Ajay <gangwar030@gmail.com>
2026-03-03 00:08:20 +0530

    Valid Palindrome String Problem 125 added Optimezed Method 2

--- Leetcode/Easy/Valid_Palindrome_Str_125.java
+++ Leetcode/Easy/Valid_Palindrome_Str_125.java
@@ -1,13 +1,15 @@
 
 class Palindrome{
 
+    // String s = "race a car";
+    // String s = "A man, a plan, a canal: Panama";
+    // String s = "Marge, let's \"[went].\" I await {news} telegram.";
+    
+    // String s = "Was it\\ a rat I saw?";
+    String s = "`l;`` 1o1 ??;l`";
+
+    // Method 1 takes time : O(n) and space O(n) and allso slower
     public boolean Method_1(){
-        // String s = "race a car";
-        // String s = "A man, a plan, a canal: Panama";
-        // String s = "Marge, let's \"[went].\" I await {news} telegram.";
-        
-        // String s = "Was it\\ a rat I saw?";
-        String s = "`l;`` 1o1 ??;l`";
 
         // String clean = s.replaceAll("[\\\\`!@#$%^&*()_:,;.<>?/\\|\"{}'--+=\\[\\]]", "").toLowerCase();
         String clean = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
@@ -25,6 +27,33 @@ class Palindrome{
         }
 
     }
+
+
+    public boolean Method_2(){
+
+        int left = 0;
+        int right = (s.length()) -1;
+
+        while (left < right) {
+            
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
 
 
@@ -33,6 +62,7 @@ public class Valid_Palindrome_Str_125{
         
         Palindrome p = new Palindrome();
 
-        System.out.println(p.Method_1());
+        // System.out.println(p.Method_1());
+        System.out.println(p.Method_2());
     }
 }

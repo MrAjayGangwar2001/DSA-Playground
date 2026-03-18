cc8a966b285a834e2be8b075391ff58777b238e5
Ajay <gangwar030@gmail.com>
2026-03-18 22:55:38 +0530

    Remove Duplicate

---
+++ Recursion/RemoveDuplicate.java
@@ -0,0 +1,25 @@
+
+public class RemoveDuplicate {
+    public static boolean []map = new boolean[26];
+
+    public static void DuplicateHtao(String str, int i, String newStr){
+
+        if (i == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char curr =  str.charAt(i);
+        if (map[curr - 'a']) {
+            DuplicateHtao(str, i+1, newStr);
+        } else {
+            newStr += curr;
+            map[curr - 'a'] = true;
+            DuplicateHtao(str, i+1, newStr);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abbabbacddcd";
+        DuplicateHtao(str, 0, "");
+    }
+}

2c452b6285abb491c9975f5ee798f29945f89e5c
Ajay <gangwar030@gmail.com>
2026-04-14 03:56:50 +0530

    Solid Square Pattern

---
+++ DSA_Series/Pattern_Printing/SolidSquare.java
@@ -0,0 +1,11 @@
+public class SolidSquare{
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

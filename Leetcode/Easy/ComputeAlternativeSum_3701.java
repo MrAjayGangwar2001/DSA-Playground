13f4a760745731f171262c4bea8947547677931f
Ajay <gangwar030@gmail.com>
2026-04-26 19:32:54 +0530

    Compute Alternative Sum prblm No. 3701

---
+++ Leetcode/Easy/ComputeAlternativeSum_3701.java
@@ -0,0 +1,38 @@
+package Leetcode.Easy;
+
+public class ComputeAlternativeSum_3701 {
+
+    static int AlternatingSum(int[] nums){
+
+        int len = nums.length;
+        int sum = 0;
+
+
+        if(len == 1){
+            return nums[0];
+        }
+
+        for (int i = 0; i < len -1; i +=2) {
+            
+            sum += nums[i] - nums[i + 1];
+        }
+
+        if (len % 2 != 0) {
+            sum += nums[len-1];
+        }
+
+        
+
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 5, 7};
+        // int nums1[] = {100};
+        int result = AlternatingSum(nums);
+        // int result1 = AlternatingSum(nums1);
+
+        System.out.println(result);
+        // System.out.println(result1);
+    }
+}

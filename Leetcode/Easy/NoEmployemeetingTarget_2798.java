e27d8ddd7fd4f0911b19165c5949791116a6f336
Ajay <gangwar030@gmail.com>
2026-05-07 20:20:58 +0530

    No of Employe who met the working target problem No. 2798

---
+++ Leetcode/Easy/NoEmployemeetingTarget_2798.java
@@ -0,0 +1,33 @@
+package Leetcode.Easy;
+
+
+
+public class NoEmployemeetingTarget_2798{
+
+    static int NoOfEmployeWhoMetTarget(int[] hours, int target){
+
+        int count = 0; 
+
+        for(int arr : hours){
+            if(arr >= target){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int [] hours = {0,1,2,3,4};
+        int target = 2;
+
+        int [] hours2 = {5,1,4,2,2};
+        int target2= 6;
+
+        int result = NoOfEmployeWhoMetTarget(hours, target);
+        int result2 = NoOfEmployeWhoMetTarget(hours, target2);
+
+        System.out.println(result);
+        System.out.println(result2);
+    }
+}

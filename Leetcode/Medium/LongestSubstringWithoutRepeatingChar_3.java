e26a4ed504882d7c8d2094851309feb25a5b8e9b
Ajay <gangwar030@gmail.com>
2026-05-30 23:32:50 +0530

    Longest Substring No Repeating char. added Optimized Method

--- Leetcode/Medium/LongestSubstringWithoutRepeatingChar_3.java
+++ Leetcode/Medium/LongestSubstringWithoutRepeatingChar_3.java
@@ -1,35 +1,48 @@
 package Leetcode.Medium;
 
-import java.util.HashMap;
-import java.util.Map;
 
 class LongestSubstringWithoutRepeatingChar_3 {
     public static int lengthOfLongestSubstring(String s) {
 
-        Map<Character, Integer> mp = new HashMap<>();
+    // METHOD 1 USING HASHMAP OPTIMISED BUT TOOK 6MS RUNTIME------------------
 
-    // Here Sliding Window Technique will be work....
-    // WINDOW SIZE ==> right - left + 1
-    //  1. Move Forward
-    //  2. Process Window Data
-    //  3. Shrink/Expand window if Required
-    //  window ===>  [ left..........right ]
-        
+        // Map<Character, Integer> mp = new HashMap<>();
 
-        int length = 0;
+        // int length = 0;
+        // int left = 0;
+
+        // for (int right = 0; right < s.length(); right++) {
+
+        //     char ch = s.charAt(right);
+
+        //     if (mp.containsKey(ch) && mp.get(ch) >= left) {
+        //         left = mp.get(ch) + 1;
+        //     }
+        //     mp.put(ch, right);
+
+        //     length = Math.max(length, right - left + 1);
+        //     // System.out.println("map.get method " +mp.get(ch));
+        // }
+
+    // METHOD 2 MORE OPTIMIZED AND EFFICIENT TOOK 1 ms RUNTIME
+
         int left = 0;
+        int length = 0;
+        int[] arr = new int[128];
 
         for (int right = 0; right < s.length(); right++) {
-
+            
             char ch = s.charAt(right);
+            System.out.println("character "+ ch);
 
-            if (mp.containsKey(ch) && mp.get(ch) >= left) {
-                left = mp.get(ch) + 1;
-            }
-            mp.put(ch, right);
-
+            left = Math.max(left, arr[ch]);
+            System.out.println("left - Max "+ left);
+            
             length = Math.max(length, right - left + 1);
-            // System.out.println("map.get method " +mp.get(ch));
+            System.out.println("length - Max "+ length);
+            
+            arr[ch] = right + 1;
+            System.out.println("arr[ch] "+ arr[ch]);
         }
 
         return length;
@@ -37,10 +50,9 @@ class LongestSubstringWithoutRepeatingChar_3 {
     }
 
     public static void main(String[] args) {
-        // int result = lengthOfLongestSubstring("abcabcbb");
+        int result = lengthOfLongestSubstring("abcabcbb");
         // int result = lengthOfLongestSubstring("bbbbbb");
-        int result = lengthOfLongestSubstring("bbaacc");
+        // int result = lengthOfLongestSubstring("bbaacc");
         System.out.println(result);
     }
 }
-

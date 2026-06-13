2726009b71d27e9d2f89684794191ec38ff3201a
Ajay <gangwar030@gmail.com>
2026-06-13 22:48:04 +0530

    Find Score of An Array After Marking all the Elements problem No. 2593

---
+++ Leetcode/Medium/FindScoreOfArrayAfterMarkingAll_2593.java
@@ -0,0 +1,58 @@
+package Leetcode.Medium;
+
+import java.util.Arrays;
+
+public class FindScoreOfArrayAfterMarkingAll_2593 {
+
+    public static long FindScore(int[] nums) {
+
+        int len = nums.length;
+
+        boolean[] arr = new boolean[len];
+        Arrays.fill(arr, true);
+
+        int[][] pair = new int[len][2];
+
+        for (int i = 0; i < len; i++) {
+
+            pair[i][0] = nums[i];
+            pair[i][1] = i;
+        }
+
+        Arrays.sort(pair, (a, b) -> Integer.compare(a[0], b[0]));
+
+        long sum = 0;
+        for (int j = 0; j < len; j++) {
+
+            int value = pair[j][0];
+            int idx = pair[j][1];
+
+            if (!arr[idx])
+                continue;
+
+            sum += value;
+
+            if (idx > 0)
+                arr[idx - 1] = false;
+
+            if (idx < len - 1)
+                arr[idx + 1] = false;
+
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        
+        // input = { 4, 1, 6, 2, 3, 5 };
+        // output = 1 + 2 + 5 = 8
+
+        int[] nums = { 4, 1, 6, 2, 3, 5 };
+       
+
+        long result = FindScore(nums);
+
+        System.out.print(result);
+    }
+}

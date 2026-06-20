70984405f69ec76a4c4de79f34a56287009d4686
Ajay <gangwar030@gmail.com>
2026-06-20 19:07:51 +0530

    Find Minimum time to Visiting All Points problem no. 1266 cleaner version

--- Leetcode/DailyQS/MinTimeVisitingAllPoints_1266.java
+++ Leetcode/DailyQS/MinTimeVisitingAllPoints_1266.java
@@ -8,22 +8,38 @@ public class MinTimeVisitingAllPoints_1266 {
         int len = points.length;
 
         
-        for (int i = 1; i < len ; i++) {
+        // for (int i = 1; i < len ; i++) {
             
-            int dx = points[i - 1][0];
-            int dy = points[i - 1][1];
+        //     int dx = points[i - 1][0];
+        //     int dy = points[i - 1][1];
         
         
-            int time1 = Math.abs(dx -  points[i][0]);
-            int time2 = Math.abs(dy - points[i][1]);
+        //     int time1 = Math.abs(dx -  points[i][0]);
+        //     int time2 = Math.abs(dy - points[i][1]);
         
         
         
-            minTime += Math.max(time1, time2);
-        }
+        //     minTime += Math.max(time1, time2);
+        // }
     
 
+//    MORE CLEANER AND READABLE
+        for (int i = 1; i < len ; i++) {
 
+            int x1 = points[i - 1][0];
+            int y1 = points[i - 1][1];
+
+            int x2 = points[i][0];
+            int y2 = points[i][1];
+
+
+            int dx = Math.abs(x1 -  x2);
+            int dy = Math.abs(y1 - y2);
+            
+
+            
+            minTime += Math.max(dx, dy);
+        }
 
         return minTime;
     }

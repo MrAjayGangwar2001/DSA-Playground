2b61baa038456be16f477ec8384733e788c1ed36
Ajay <gangwar030@gmail.com>
2026-06-29 23:39:19 +0530

    Number Of String Appear As Substring In Word problem 1967

---
+++ Leetcode/DailyQS/NoOfStringAppearAsSubstringInWord_1967.java
@@ -0,0 +1,37 @@
+package Leetcode.DailyQS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoOfStringAppearAsSubstringInWord_1967 {
+
+    public static void main(String[] args) {
+        
+        String word = "abc";
+
+        String[] pattern = {"a","abc","bc","d"};
+
+        int Result = NoOfSubString(pattern, word);
+
+        System.out.println(Result);
+
+    }
+
+    private static int NoOfSubString(String[] pattern, String word) {
+       
+        int count = 0;
+
+        for(String s : pattern){
+
+            boolean check = word.contains(s);
+
+            if (check) {
+                count++;
+            }
+
+
+            
+        }
+        return count;
+    }
+}

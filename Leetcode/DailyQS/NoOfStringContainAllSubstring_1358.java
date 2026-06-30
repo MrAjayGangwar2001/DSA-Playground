2b94d3cc6eac07893f447ff064131ddf976e29f2
Ajay <gangwar030@gmail.com>
2026-07-01 00:39:58 +0530

    Return the Number of subString which is contains in a string/word Added another Method

--- Leetcode/DailyQS/NoOfStringContainAllSubstring_1358.java
+++ Leetcode/DailyQS/NoOfStringContainAllSubstring_1358.java
@@ -6,41 +6,66 @@ public class NoOfStringContainAllSubstring_1358 {
 
         String str = "aaacb";
         // String str = "abcabc";
-        
-        int Result = CountNoOfSubstring(str);
 
+        int Result = CountNoOfSubstring(str);
 
         System.out.println(Result);
 
     }
 
     private static int CountNoOfSubstring(String str) {
-       
 
-           int len = str.length();
+        int len = str.length();
+
+    //  Basic Bruteforce Approach but return TLE Error with O(n^2/ n^3)
+    /*
+         * int count = 0;
+         * for (int i = 0; i < str.length(); i++) {
+         * 
+         * StringBuilder sb = new StringBuilder();
+         * for (int j = i; j < str.length(); j++) {
+         * 
+         * sb.append(str.charAt(j));
+         * 
+         * String x = "a";
+         * String y = "b";
+         * String z = "c";
+         * 
+         * if (sb.toString().contains(x) && sb.toString().contains(y) &&
+         * sb.toString().contains(z)) {
+         * count++;
+         * 
+         * }
+         * }
+         * 
+         * }
+         */
 
         int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-        
-            StringBuilder sb = new StringBuilder();
-            for (int j = i; j < str.length(); j++) {
-        
-                sb.append(str.charAt(j));
-        
-                String x = "a";
-                String y = "b";
-                String z = "c";
-        
-                if (sb.toString().contains(x) && sb.toString().contains(y) && sb.toString().contains(z)) {
-                    count++;
-        
+        for (int i = 0; i < len; i++) {
+
+            boolean hasA = false;
+            boolean hasB = false;
+            boolean hasC = false;
+            for (int j = i; j < len; j++) {
+
+                char ch = str.charAt(j);
+
+                if (ch == 'a')
+                    hasA = true;
+                else if (ch == 'b')
+                    hasB = true;
+                else if (ch == 'c')
+                    hasC = true;
+
+                if (hasA && hasB && hasC) {
+                    count += len - j;
+                    break;
                 }
             }
-        
-           
         }
-        
-       
+
         return count;
+
     }
 }

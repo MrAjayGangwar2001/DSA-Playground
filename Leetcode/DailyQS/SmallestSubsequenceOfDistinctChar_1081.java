519437bc3c82e71efa03e158bdd4621f78dcddd5
Ajay <gangwar030@gmail.com>
2026-07-20 00:25:13 +0530

    Smallest Subsequence of Distinct character problem no. 1081

---
+++ Leetcode/DailyQS/SmallestSubsequenceOfDistinctChar_1081.java
@@ -0,0 +1,62 @@
+package Leetcode.DailyQS;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class SmallestSubsequenceOfDistinctChar_1081 {
+
+    public static void main(String[] args) {
+
+        String s = "cbacdcbc";
+
+        String Result = SmallestSubSequence(s);
+
+        System.out.println(Result);
+
+    }
+
+    private static String SmallestSubSequence(String s) {
+        Map<Character, Integer> lastIdx = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            lastIdx.put(ch, i);
+        }
+
+        // System.out.println(lastIdx);
+
+        Stack<Character> sk = new Stack<>();
+
+        boolean[] visited = new boolean[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (visited[ch - 'a']) {
+                continue;
+            }
+            while (!sk.isEmpty() && (ch - 'a') < (sk.peek() - 'a') && lastIdx.get(sk.peek()) > i) {
+
+                char c = sk.pop();
+                visited[c - 'a'] = false;
+            }
+
+            if (!visited[ch - 'a']) {
+
+                sk.push(ch);
+            }
+            visited[ch - 'a'] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!sk.isEmpty()) {
+            sb.append(sk.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}

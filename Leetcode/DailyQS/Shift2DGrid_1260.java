380707215741f6c40174e7ea5192ea87304bc06d
Ajay <gangwar030@gmail.com>
2026-07-21 22:03:19 +0530

    Shift 2D Array problem No. 1260

---
+++ Leetcode/DailyQS/Shift2DGrid_1260.java
@@ -0,0 +1,49 @@
+package Leetcode.DailyQS;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Shift2DGrid_1260 {
+
+    public static void main(String[] args) {
+        
+        int[][] grid = {
+            {1, 2 , 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        List<Integer> ls = new ArrayList<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            
+            int colLen = grid[i].length;
+
+            for (int j = 0; j < colLen; j++) {
+                
+                ls.add(grid[i][j]);
+            }
+        }
+
+        Collections.rotate(ls, 1);
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        int idx = 0;
+       for (int i = 0; i < grid.length; i++) {
+           int colLen = grid[i].length;
+            List<Integer> row = new ArrayList<>();
+           for (int j = 0; j < colLen; j++) {
+               
+                row.add(ls.get(idx++));
+           }
+
+           list.add(row);
+       }
+
+       System.out.println(list);
+
+    //    return list;
+    }
+}

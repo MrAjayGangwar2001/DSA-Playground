c296fe651eda9d164f6b75de2a042f40245b6108
Ajay <gangwar030@gmail.com>
2026-07-21 22:17:18 +0530

    Shift 2D Array problem No. 1260

--- Leetcode/DailyQS/Shift2DGrid_1260.java
+++ Leetcode/DailyQS/Shift2DGrid_1260.java
@@ -7,21 +7,30 @@ import java.util.List;
 public class Shift2DGrid_1260 {
 
     public static void main(String[] args) {
-        
+
         int[][] grid = {
-            {1, 2 , 3},
-            {4, 5, 6},
-            {7, 8, 9}
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
+
+        List<List<Integer>> ls = ShiftedGrid(grid);
+        System.out.println(ls);
+    }
+
+    private static java.util.List<java.util.List<Integer>> ShiftedGrid(int[][] grid) {
+     
+
+    // public Static List<List<Integer>> ShiftedGrid(int[][] grid) {
 
         List<Integer> ls = new ArrayList<>();
 
         for (int i = 0; i < grid.length; i++) {
-            
+
             int colLen = grid[i].length;
 
             for (int j = 0; j < colLen; j++) {
-                
+
                 ls.add(grid[i][j]);
             }
         }
@@ -31,19 +40,19 @@ public class Shift2DGrid_1260 {
         List<List<Integer>> list = new ArrayList<>();
 
         int idx = 0;
-       for (int i = 0; i < grid.length; i++) {
-           int colLen = grid[i].length;
+        for (int i = 0; i < grid.length; i++) {
+            int colLen = grid[i].length;
             List<Integer> row = new ArrayList<>();
-           for (int j = 0; j < colLen; j++) {
-               
+            for (int j = 0; j < colLen; j++) {
+
                 row.add(ls.get(idx++));
-           }
+            }
 
-           list.add(row);
-       }
+            list.add(row);
+        }
 
-       System.out.println(list);
+        // System.out.println(list);
 
-    //    return list;
+        return list;
     }
 }

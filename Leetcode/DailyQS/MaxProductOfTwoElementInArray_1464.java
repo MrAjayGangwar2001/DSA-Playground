6d276dcb6c3cc7fa94582e421074f65b2484dff2
Ajay <gangwar030@gmail.com>
2026-07-28 01:04:27 +0530

    Maximum Product of Two Element in An Arrayproblem No. 1464

--- Leetcode/DailyQS/MaxProductOfTwoElementInArray_1464.java
+++ Leetcode/DailyQS/MaxProductOfTwoElementInArray_1464.java
@@ -10,8 +10,10 @@ public class MaxProductOfTwoElementInArray_1464 {
         int[] nums = {1,5,4,5};
 
         int ans = MaxProductM1(nums);
+        int ans2 = MaxProductM2(nums);
 
         System.out.println(ans);
+        System.out.println(ans2);
 
     }
 
@@ -26,6 +28,25 @@ public class MaxProductOfTwoElementInArray_1464 {
         return ans;
     }
 
-    
+    private static int MaxProductM2(int[] nums) {
+
+        int len = nums.length;
+
+        int max = 0, max2 = 0;
+
+        for (int num : nums) {
+            
+            if (num >= max) {
+                max2 = max;
+                max = num;
+            } else if(num >= max2){
+                max2 = num;
+            }
+        }
+
+        int ans = (max - 1) * (max2 - 1);
+
+        return ans;
+    }
 
 }

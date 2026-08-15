3a32305732209ddf72e6aa022fa1d7c38ca3a2a9
Ajay <gangwar030@gmail.com>
2026-08-15 23:56:55 +0530

    Longest Subsequence With Non Zero Bitwise XOR problem No. 3702 Added Another Method

--- Leetcode/DailyQS/LongestSubsequenceWithNonZeroBitwiseXOR_3702.java
+++ Leetcode/DailyQS/LongestSubsequenceWithNonZeroBitwiseXOR_3702.java
@@ -5,11 +5,13 @@ public class LongestSubsequenceWithNonZeroBitwiseXOR_3702 {
     public static void main(String[] args) {
 
         // int[] nums = { 2, 3, 4 };
-        int[] nums = {1, 2, 3 };
+        int[] nums = { 1, 2, 3 };
 
         int Result = LongestSubsequenceWithNonZero(nums);
+        int Method2 = longestSubsequence(nums);
 
         System.out.println(Result);
+        System.out.println(Method2);
     }
 
     private static int LongestSubsequenceWithNonZero(int[] nums) {
@@ -39,7 +41,33 @@ public class LongestSubsequenceWithNonZeroBitwiseXOR_3702 {
         return xor;
     }
 
+    public static int longestSubsequence(int[] nums) {
 
+        int n = nums.length;
+        int xor = 0;
+        boolean hasNonZero = false;
 
-    
+        for (int num : nums) {
+            xor ^= num;
+
+            if (num != 0) {
+                hasNonZero = true;
+            }
+        }
+
+        // Entire array has non-zero XOR
+        if (xor != 0) {
+            return n;
+        }
+
+        // Entire array XOR is 0,
+        // but at least one non-zero element exists
+        if (hasNonZero) {
+            return n - 1;
+        }
+
+        // All elements are 0
+        return 0;
+    }
+
 }
